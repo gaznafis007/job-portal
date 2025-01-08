0e87f5c8243479382760dc73b2ced1f500ae400d
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-09 01:33:52 +0600

    navbar with responsiveness

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,39 +1,70 @@
 import { Link } from "react-router-dom";
 import Button from "../../components/Button";
-
+import { HiMiniBars3 } from "react-icons/hi2";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
-    const navItems = [
-        {
-            name: 'home',
-            path: '/'
-        },
-        {
-            name: 'add job',
-            path: '/addJob'
-        },
-        {
-            name: 'all jobs',
-            path: '/jobs'
-        }
-    ]
-    return (
-        <nav className="p-6 flex flex-row justify-between border border-zinc-300 rounded-md m-3">
-          <h2 className="text-3xl text-cyan-400 font-semibold">Job Portal</h2>  
-          <ul className="flex flex-row space-x-2 text-xl capitalize">
-            {
-                navItems?.map((item, idx) =>(
-                    <li key={idx}><Link to={item?.path}>{item?.name}</Link></li>
-                ))
-            }
-          </ul>
-          <div>
+  const [open, setOpen] = useState(false);
+  const navItems = [
+    {
+      name: "home",
+      path: "/",
+    },
+    {
+      name: "add job",
+      path: "/addJob",
+    },
+    {
+      name: "all jobs",
+      path: "/jobs",
+    },
+  ];
+  return (
+    <nav className="p-6 flex flex-row justify-between border border-zinc-300 rounded-md m-3 items-center">
+      <h2 className="text-3xl text-cyan-400 font-semibold">Job Portal</h2>
+      <ul className="hidden md:flex flex-row space-x-2 text-xl capitalize">
+        {navItems?.map((item, idx) => (
+          <li key={idx}>
+            <Link to={item?.path}>{item?.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="hidden md:block">
+        <Button>
+          <Link to={"/signin"}> Sign in</Link>
+        </Button>
+      </div>
+      <div className="flex flex-col space-y-3 md:hidden">
+      {open ? (
+        <RxCross2
+          className="cursor-pointer text-xl text-cyan-400 self-end"
+          onClick={() => setOpen(false)}
+        />
+      ) : (
+        <HiMiniBars3
+          className="cursor-pointer text-xl text-cyan-400"
+          onClick={() => setOpen(true)}
+        />
+      )}
+      {open && (
+        <ul className="flex flex-col space-y-2 text-xl capitalize">
+          {navItems?.map((item, idx) => (
+            <li key={idx}>
+              <Link to={item?.path}>{item?.name}</Link>
+            </li>
+          ))}
+          <li>
             <Button>
-                <Link to={'/signin'}> Sign in</Link>
+              <Link to={"/signin"}> Sign in</Link>
             </Button>
-          </div>
-        </nav>
-    );
+          </li>
+        </ul>
+      )}
+      </div>
+      
+    </nav>
+  );
 };
 
 export default Navbar;
