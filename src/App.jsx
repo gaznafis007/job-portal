8773b0606c39238bc83656559e6448136855289d
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-09 01:47:10 +0600

    axios init and store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes/router"
+import { Provider } from "react-redux"
+import { store } from "./redux/store"
 
 
 function App() {
   
 
   return (
-    <>
+    <Provider store={store}>
       <RouterProvider router={router}/>
-    </>
+    </Provider>
   )
 }
 
